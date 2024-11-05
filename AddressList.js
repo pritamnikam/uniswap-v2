3a3895891c0ed974d60ffb30f5f4c2a989f1b641
Pritam Nikam <pritam.nikam@gmail.com>
2024-11-06 00:38:49 +0530

    Initial commit.

---
+++ AddressList.js
@@ -0,0 +1,11 @@
+const factoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+const routerAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const fromAddress = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"; // WETH
+const toAddress = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"; // DIA
+
+module.exports = {
+    factoryAddress,
+    routerAddress,
+    fromAddress,
+    toAddress
+}
